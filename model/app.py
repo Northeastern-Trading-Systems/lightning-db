1ba725d22d00fa3b3a7c14eac33162156a6f6f2a
Allen Lin <allenlinsh@gmail.com>
2023-01-03 13:25:07 -0500

    Change variable name for gunicorn

--- model/app.py
+++ model/app.py
@@ -7,11 +7,11 @@
 from src import create_app
 
 # create the app object
-app = create_app()
+application = create_app()
 
 if __name__ == '__main__':
     # we want to run in debug mode (for hot reloading) 
     # this app will be bound to port 4000. 
     # Take a look at the docker-compose.yml to see 
     # what port this might be mapped to... 
-    app.run(debug = False, host = '0.0.0.0', port = 4000)
+    application.run(debug = False, host = '0.0.0.0', port = 4000)
